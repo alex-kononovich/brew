2dd8ebcefd75188fad1a0f97f6a6be82318f0567
Alex K <flskif@gmail.com>
2019-12-13 14:32:24 -0800

    Add session-manager-plugin dependency

--- lp-webapp.rb
+++ lp-webapp.rb
@@ -1,12 +1,14 @@
 class LpWebapp < Formula
-  desc "Connect to lp-webapp EC2 instances"
-  homepage "https://github.com/alex-kononovich/brew"
-  head "https://raw.githubusercontent.com/alex-kononovich/brew/master/lp-webapp"
+  desc 'Connect to lp-webapp EC2 instances'
+  homepage 'https://github.com/alex-kononovich/brew'
 
-  depends_on "awscli"
-  depends_on "fzf"
+  head 'https://raw.githubusercontent.com/alex-kononovich/brew/master/lp-webapp'
+
+  depends_on 'awscli'
+  depends_on 'fzf'
+  depends_on 'https://raw.githubusercontent.com/dkanejs/homebrew-aws-session-manager-plugin/master/Formula/aws-session-manager-plugin.rb'
 
   def install
-    bin.install "lp-webapp"
+    bin.install 'lp-webapp'
   end
 end
